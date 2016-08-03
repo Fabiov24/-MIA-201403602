e5b215b75962fa787ff74018a130a284bfe2f04e
Fabiov24 <fabio.vasquez.24@gmail.com>
2016-08-02 19:29:02 -0600

    Pseudo analizador

--- Fase1/main.c
+++ Fase1/main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/*
-Sección para variables
-*/
-char Entrada[400];
 
+//---------------------------------------------------------------------------------
+char Entrada[400];
+//---------------------------------------------------------------------------------
+
+void AnalizarEntrada(char Texto[]){ //método para analizar los comandos de entrada
+    printf("%s\n",Texto);
+    char *completo;
+
+    completo = strtok (Texto," \\");
+    while (completo != NULL)
+    {
+        printf ("%s\n",completo);
+
+        if(strcasecmp(completo, "mkdisk") !=0){
+            //reconocer acá los parámetros del mkdisk
+
+
+        }else if(strcasecmp(completo, "rmdisk") !=0){
+            //reconocer acá los parámetros del rmdisk
+        }
+
+
+        completo = strtok (NULL, " \\");
+    }
+}
 
 
 
@@ -27,9 +48,13 @@ int main()
             char EntradaAnterior[strlen(Entrada)] ;
             strcpy(EntradaAnterior,Entrada);
             scanf(" %[^\n]s", Entrada);
-            printf("Se leyó el segundo %s %s \n",EntradaAnterior,Entrada);
-        }else{
+            printf("Se leyó el segundo %s %s \n",EntradaAnterior,Entrada);//línea de prueba :v
+            strcat(EntradaAnterior," ");
+            strcat(EntradaAnterior,Entrada);
 
+            AnalizarEntrada(EntradaAnterior);
+        }else{
+            //acá debería mandar a analizar el texto
         }
     }while(strcasecmp(Entrada, "exit") !=0);
     return 0;
